d8613292e04b3f3358c926bdec9aaedc26547996
Sven Gonschorek <sven.gonschorek@bi-solutions-hamburg.com>
2024-07-18 17:17:59 +0200

    master: initial script plus first models

---
+++ models/vault/raw/hub_crypto_payin.sql
@@ -0,0 +1,32 @@
+{{
+    config(
+        materialized = 'table',
+        order_by = 'pk_crypto_payin'
+    )
+}}
+
+-- select data from sources
+-----------------------------------------------
+with binance_payins as (
+
+    select * from {{ source('binance', 'binance_payins') }}
+
+),
+
+-- implement logic to build the model
+-----------------------------------------------
+final as (
+
+    select
+        -- keys
+        lower(hex(MD5(concat('binance_', order_id)))) as pk_crypto_payin,
+        order_id as bk_crypto_payin,
+        -- metadata
+        '{{ invocation_id }}' as record_source,
+        toDateTime(now(), 'Europe/Berlin') as load_dts
+    from binance_payins
+    where status = 'Successful'
+
+)
+
+select * from final
---
+++ models/vault/raw/sat_crypto_payin.sql
@@ -0,0 +1,38 @@
+{{
+    config(
+        materialized = 'table',
+        order_by = 'fk_crypto_payin'
+    )
+}}
+
+-- select data from sources
+-----------------------------------------------
+with binance_payins as (
+
+    select * from {{ source('binance', 'binance_payins') }}
+
+),
+
+-- implement logic to build the model
+-----------------------------------------------
+final as (
+
+    select
+        -- keys
+        lower(hex(MD5(concat('binance_', order_id)))) as fk_crypto_payin,
+        -- metadata
+        '{{ invocation_id }}' as record_source,
+        toDateTime(now(), 'Europe/Berlin') as load_dts,
+        -- properties
+        'binance' as platform,
+        toDateTime(payin_local_at) as payin_at,
+        toDecimal32(amount, 2) as amount,
+        toDecimal32(fee, 2) as fee_amount,
+        coin,
+        payment_method
+    from binance_payins
+    where status = 'Successful'
+
+)
+
+select * from final
