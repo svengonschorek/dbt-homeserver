f100d53f018e86e5f114b352b1db1ed7eb004ff9
Sven Gonschorek <sven.gonschorek@bi-solutions-hamburg.com>
2026-04-10 23:13:46 +0200

    pnl: add sat model

---
+++ models/vault/raw/sat_crypto_pnl.sql
@@ -0,0 +1,33 @@
+{{
+    config(
+        materialized = 'table',
+        order_by = 'fk_crypto_pnl'
+    )
+}}
+
+-- select data from sources
+-----------------------------------------------
+with crypto_pnl as (
+
+    select * from {{ source('bybit', 'bybit_pnl') }}
+
+),
+
+-- implement logic to build the model
+-----------------------------------------------
+final as (
+
+    select
+        -- keys
+        lower(hex(MD5(concat('bybit_', symbol, '_', orderId)))) as fk_crypto_pnl,
+        -- metadata
+        '{{ invocation_id }}' as record_source,
+        toDateTime(now(), 'Europe/Berlin') as load_dts,
+        -- properties
+        "bybit" as platform,
+        symbol,
+    from crypto_pnl
+
+)
+
+select * from final
