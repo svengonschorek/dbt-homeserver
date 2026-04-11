a42dd671fd7c8c0fcf62bf5ef72299f4d0fc8270
Sven Gonschorek <sven.gonschorek@bi-solutions-hamburg.com>
2026-04-11 15:28:01 +0200

    pnl: add property columns and data tests

--- models/vault/raw/sat_crypto_pnl.sql
+++ models/vault/raw/sat_crypto_pnl.sql
@@ -24,8 +24,17 @@ final as (
         '{{ invocation_id }}' as record_source,
         toDateTime(now(), 'Europe/Berlin') as load_dts,
         -- properties
-        "bybit" as platform,
+        'bybit' as platform,
         symbol,
+        side,
+        orderType as order_type,
+        toDecimal64(qty, 4) as quantity,
+        toDecimal64(avgEntryPrice, 8) as avg_entry_price,
+        toDecimal64(cumEntryValue, 8) as cum_entry_value,
+        toDecimal64(avgExitPrice, 8) as avg_exit_price,
+        toDecimal64(cumExitValue, 8) as cum_exit_value,
+        toDecimal64(orderPrice, 8) as order_price,
+        toDecimal64(closedPnl, 8) as closed_pnl
     from crypto_pnl
 
 )
