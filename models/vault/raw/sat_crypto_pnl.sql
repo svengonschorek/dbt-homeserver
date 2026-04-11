bbecf9ce78ac73c25583ec8ce0978e02bdec23f5
Sven Gonschorek <sven.gonschorek@bi-solutions-hamburg.com>
2026-04-11 15:31:47 +0200

    master: add sqlfluff fixes

--- models/vault/raw/sat_crypto_pnl.sql
+++ models/vault/raw/sat_crypto_pnl.sql
@@ -19,23 +19,23 @@ final as (
 
     select
         -- keys
-        lower(hex(MD5(concat('bybit_', symbol, '_', orderId)))) as fk_crypto_pnl,
+        lower(hex(MD5(concat('bybit_', pnl.symbol, '_', pnl.orderId)))) as fk_crypto_pnl,
         -- metadata
         '{{ invocation_id }}' as record_source,
         toDateTime(now(), 'Europe/Berlin') as load_dts,
         -- properties
         'bybit' as platform,
-        symbol,
-        side,
-        orderType as order_type,
-        toDecimal64(qty, 4) as quantity,
-        toDecimal64(avgEntryPrice, 8) as avg_entry_price,
-        toDecimal64(cumEntryValue, 8) as cum_entry_value,
-        toDecimal64(avgExitPrice, 8) as avg_exit_price,
-        toDecimal64(cumExitValue, 8) as cum_exit_value,
-        toDecimal64(orderPrice, 8) as order_price,
-        toDecimal64(closedPnl, 8) as closed_pnl
-    from crypto_pnl
+        pnl.symbol,
+        pnl.side,
+        pnl.orderType as order_type,
+        toDecimal64(pnl.qty, 4) as quantity,
+        toDecimal64(pnl.avgEntryPrice, 8) as avg_entry_price,
+        toDecimal64(pnl.cumEntryValue, 8) as cum_entry_value,
+        toDecimal64(pnl.avgExitPrice, 8) as avg_exit_price,
+        toDecimal64(pnl.cumExitValue, 8) as cum_exit_value,
+        toDecimal64(pnl.orderPrice, 8) as order_price,
+        toDecimal64(pnl.closedPnl, 8) as closed_pnl
+    from crypto_pnl as pnl
 
 )
 
