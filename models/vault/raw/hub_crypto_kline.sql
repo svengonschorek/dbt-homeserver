7f14aaffac654eb65b09220ae17a7aca66f34932
Sven Gonschorek <sven.gonschorek@bi-solutions-hamburg.com>
2026-04-16 16:56:11 +0200

    klines: add source and hub model for klines

---
+++ models/vault/raw/hub_crypto_kline.sql
@@ -0,0 +1,45 @@
+{{
+    config(
+        materialized='table',
+        order_by='pk_crypto_kline'
+    )
+}}
+
+-- select data from sources
+-----------------------------------------------
+with bybit_klines_1m_usdt_eur as (
+
+    select * from {{ source('bybit', 'bybit_kline_1m_usdt_eur') }}
+
+),
+
+-- implement logic to build the model
+-----------------------------------------------
+base_bybit as (
+
+    select
+        concat('EURUSDT_1m_', klineAt) as unique_key,
+        row_number() over (
+            partition by klineAt order by volume desc
+        ) as r
+    from bybit_klines_1m_usdt_eur
+
+),
+
+-- final model
+-----------------------------------------------
+final as (
+
+    select
+        -- keys
+        lower(hex(MD5(unique_key))) as pk_crypto_kline,
+        unique_key as bk_crypto_kline,
+        -- metadata
+        '{{ invocation_id }}' as record_source,
+        toDateTime(now(), 'Europe/Berlin') as load_dts
+    from base_bybit
+    where r = 1
+
+)
+
+select * from final
